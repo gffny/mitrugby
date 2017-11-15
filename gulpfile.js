1ea71794bbfbc98f1e4a0b214b8005b6313a8562
John D. Gaffney <john.gaffney@isobar.com>
2017-11-15 18:16:37 -0500

    Adding gulpfile for continuous deployment of files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
+
+
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+
+};
+
+gulp.task('runKeystone', shell.task('node keystone'));
+gulp.task('watch', [
+
+]);
+
+gulp.task('default', ['watch', 'runKeystone']);
