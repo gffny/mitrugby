c66975faa88a60465a16c8e6c2f059f5b807efdd
John D. Gaffney <john.gaffney@isobar.com>
2017-11-24 18:29:43 -0500

    Removed github account connectivity for User registration

--- models/User.js
+++ models/User.js
@@ -26,14 +26,12 @@ User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, index: true },
 	password: { type: Types.Password, initial: true },
-	resetPasswordKey: { type: String, hidden: true }
-}, 'Profile', {
+	resetPasswordKey: { type: String, hidden: true } }, 'Profile', {
 	isPublic: { type: Boolean, default: true },
 	isOrganiser: Boolean,
     isGroup: Boolean,
 	organisation: { type: Types.Relationship, ref: 'Organisation' },
 	photo: { type: Types.CloudinaryImage },
-	github: { type: String, width: 'short' },
 	twitter: { type: String, width: 'short' },
 	website: { type: Types.Url },
 	bio: { type: Types.Markdown },
@@ -41,33 +39,13 @@ User.add({
 }, 'Notifications', {
 	notifications: {
 		posts: { type: Boolean },
-		meetups: { type: Boolean, default: true }
-	}
-}, 'Mentoring', {
-	mentoring: {
-		available: { type: Boolean, label: 'Is Available', index: true },
-		free: { type: Boolean, label: 'For Free', dependsOn: deps.mentoring },
-		paid: { type: Boolean, label: 'For Payment', dependsOn: deps.mentoring },
-		swap: { type: Boolean, label: 'For Swap', dependsOn: deps.mentoring },
-		have: { type: String, label: 'Has...', dependsOn: deps.mentoring },
-		want: { type: String, label: 'Wants...', dependsOn: deps.mentoring }
+		matches: { type: Boolean, default: true }
 	}
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can Admin MIT Rugby' },
 	isVerified: { type: Boolean, label: 'Has a verified email address' }
 }, 'Services', {
 	services: {
-		github: {
-			isConfigured: { type: Boolean, label: 'GitHub has been authenticated' },
-
-			profileId: { type: String, label: 'Profile ID', dependsOn: deps.github },
-
-			username: { type: String, label: 'Username', dependsOn: deps.github },
-			avatar: { type: String, label: 'Image', dependsOn: deps.github },
-
-			accessToken: { type: String, label: 'Access Token', dependsOn: deps.github },
-			refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.github }
-		},
 		facebook: {
 			isConfigured: { type: Boolean, label: 'Facebook has been authenticated' },
 
@@ -132,7 +110,7 @@ User.schema.pre('save', function(next) {
 
 User.relationship({ ref: 'Post', refPath: 'author', path: 'posts' });
 User.relationship({ ref: 'Talk', refPath: 'who', path: 'talks' });
-User.relationship({ ref: 'RSVP', refPath: 'who', path: 'rsvps' });
+User.relationship({ ref: 'Attendance', refPath: 'who', path: 'rsvps' });
 
 
 /**
@@ -153,7 +131,6 @@ User.schema.virtual('canAccessKeystone').get(function() {
 // Pull out avatar image
 User.schema.virtual('avatarUrl').get(function() {
 	if (this.photo.exists) return this._.photo.thumbnail(120,120);
-	if (this.services.github.isConfigured && this.services.github.avatar) return this.services.github.avatar;
 	if (this.services.facebook.isConfigured && this.services.facebook.avatar) return this.services.facebook.avatar;
 	if (this.services.google.isConfigured && this.services.google.avatar) return this.services.google.avatar;
 	if (this.services.twitter.isConfigured && this.services.twitter.avatar) return this.services.twitter.avatar;
@@ -163,9 +140,6 @@ User.schema.virtual('avatarUrl').get(function() {
 // Usernames
 User.schema.virtual('twitterUsername').get(function() {
 	return (this.services.twitter && this.services.twitter.isConfigured) ? this.services.twitter.username : '';
-});
-User.schema.virtual('githubUsername').get(function() {
-	return (this.services.github && this.services.github.isConfigured) ? this.services.github.username : '';
 });
 
 
