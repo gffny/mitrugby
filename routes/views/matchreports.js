4d4822bc8f4c8c6ecd89593a1e3e5b77d7dcd6ab
John D. Gaffney <john.gaffney@isobar.com>
2017-11-27 21:30:50 -0500

    Code formatting cleanup

--- routes/views/matchreports.js
+++ routes/views/matchreports.js
@@ -4,22 +4,26 @@ var keystone = require('keystone'),
 var MatchReport = keystone.list('MatchReport');
 
 exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res),
+
+    var view = new keystone.View(req, res),
 		locals = res.locals;
 	
-	locals.section = 'matchreports';
-	locals.page.title = 'Match Reports - MIT Men\'s Rugby Football Club';
+	locals.section = 'matchReports';
+	locals.page.title = 'MITRFC | Match Reports';
+
+    view.query('previousMatchReport',
+        MatchReport.model.findOne()
+            .where('state', 'active')
+            .sort('-publishedDate'));
 	
 	view.query('matchReports',
         MatchReport.model.findOne()
-			.sort('-publishedDate')
-	, '');
+			.sort('-publishedDate'));
 
 	view.on('render', function(next) {
-	
-		if (!req.user || !locals.latestMatchReport) return next();
-			
+
+        if (!req.user || !locals.latestMatchReport) return next();
+
 	});
 	
 	view.render('site/matchreports');
